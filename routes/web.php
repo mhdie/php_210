36e6e65515c1eafe44ae8ae741d98dec0f844fa8
mhdie <najafi172@gmail.com>
2018-08-28 15:39:23 +0430

    add routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function (){
+    return 'hi';
 });
+//Route::match(['get','post'],'/',function (){
+//    return view('home');
+//});
+Route::match(['get','post'],'/products', function () {
+    return view('product');});
+Route::match(['get','post'],'/products{id}', function () {
+    return view('product');
+});
+
+Route::match(['get','post'],'/login',function (){
+    return view('login');
+});
+
+Route::match(['get','post'],'/signup',function (){
+    return view('signup');
+});
+
+Route::match(['get','post'],'/shoppingbasket{id}',function (){
+    return view('shoppingbasket');
+});
+Route::match(['get','post'],'/user{id}',function (){
+    return view('user');
+});
+
